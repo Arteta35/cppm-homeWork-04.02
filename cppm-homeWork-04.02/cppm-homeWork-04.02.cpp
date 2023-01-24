06f02e2ad09ccb1d44128cad092a43d9427166ae
Артём Ершов <arteta35@ya.ru>
2023-01-24 16:46:09 +0300

    Переделано в соответствии с замечаниями

--- cppm-homeWork-04.02/cppm-homeWork-04.02.cpp
+++ cppm-homeWork-04.02/cppm-homeWork-04.02.cpp
@@ -30,47 +30,10 @@ public:
 		return city + ", " + street + ", " + to_string(num_house) + ", " + to_string(num_kv);
 	}
 
-	void sort(Adresses* arr, int N)
-	{
-		bool flag = false;
-		do
-		{
-			flag = false;
-			for (int i = 0; i < N - 1; i++) {
-
-				char a = arr[i].get_output()[0];
-				char b = arr[i + 1].get_output()[0];
-
-				if (a > b)
-				{
-					Adresses buf = arr[i];
-					arr[i] = arr[i + 1];
-					arr[i + 1] = buf;
-					flag = true;
-				}
-			}
-		} while (flag);
-	};
-
-	int setter(int N) {
-
-		if (N > 0) {
-			this -> N = N;
-			return N;
-
-		}
-		else {
-			cout << "Размерность некоректна!";
-			return -1;
-
-		}
-
-	}
 
 
 private:
 
-	int N = 0;
 	string city = "Неизвестно";
 	string street = "Неизвестно";
 	int num_house = 0;
@@ -78,14 +41,86 @@ private:
 
 };
 
+string city2(Adresses* arr, int i) {
+
+	string a = arr[i].get_output();
+	auto del = a.find(",");
+	int del1 = static_cast<int>(del);
+
+	string city1 = a.substr(0, del1);
+
+	return city1;
+
+};
+
+void sort(Adresses* arr, int N)
+{
+	bool flag = false;
+	do
+	{
+		
+		flag = false;
+
+		for (int i = 0; i < N - 1; i++) {
+
+			string a = city2(arr, i);
+			string b = city2(arr, i + 1);
+
+			auto len1 = a.length();
+			auto len2 = b.length();
+
+			int len11 = static_cast<int>(len1);
+			int len22 = static_cast<int>(len2);
+
+			if (a[0] > b[0]) {
+				Adresses buf = arr[i];
+				arr[i] = arr[i + 1];
+				arr[i + 1] = buf;
+				flag = true;
+
+			}
+			if (a[0] == b[0]) {
+
+				if (len11 >= len22) {
+
+					for (int j = 0; j < len11 - 1; j++) {
+						if (a[j] > b[j])
+						{
+							Adresses buf = arr[i];
+							arr[i] = arr[i + 1];
+							arr[i + 1] = buf;
+							break;
+						}
+					}
+
+				}
+				else {
+					for (int j = 0; j < len22 - 1; j++) {
+						if (a[j] > b[j])
+						{
+							Adresses buf = arr[i];
+							arr[i] = arr[i + 1];
+							arr[i + 1] = buf;
+							break;
+						}
+					}
+
+				}
+
+			}
+
+		}
+	} while (flag);
+};
+
+
+
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-
-	Adresses adres;
 
 	int N;
 	string city = "Неизвестно";
@@ -99,9 +134,15 @@ int main()
 	if (fin.is_open() && fout.is_open()) {
 
 		fin >> N;
+		if (N > 0) {
+			N = N;
+		}
+		else {
+			cout << "Размерность некоректна!";
+			return -1;
+
+		}
 		fout << N << std::endl;
-		string adress;
-		N = adres.setter(N);
 		Adresses* arr = new Adresses[N]();
 
 		for (int i = 0; i < N; i++)
@@ -116,8 +157,8 @@ int main()
 			arr[i].in_num_kv(num_kv);
 
 		}
-
-		adres.sort(arr, N);
+		
+		sort(arr, N);
 
 		for (int j = 0; j < N; j++) { fout << arr[j].get_output() << std::endl; }
 
